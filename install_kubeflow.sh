4bccc39a9acaf9f18774ced65b277513a1c4773d
William Xiang <william.xiang@ibm.com>
2025-04-01 17:11:49 -0400

    don't install and configure nfd
    Signed-off-by: William Xiang <william.xiang@ibm.com>

--- install_kubeflow.sh
+++ install_kubeflow.sh
@@ -278,8 +278,8 @@ case "$install_operators" in
         # Install subscriptions (operators from OperatorHub)
         while ! kustomize build $KUBEFLOW_KUSTOMIZE/subscriptions | awk '!/well-defined/' | oc apply -f -; do echo -e "Retrying to apply resources for Cert Manager..."; sleep 10; done
 
-        # Configure node feature discovery
-        while ! kustomize build $KUBEFLOW_KUSTOMIZE/nfd | awk '!/well-defined/' | oc apply -f -; do echo -e "Retrying to apply resources for Node Feature Discovery..."; sleep 10; done
+        # # Configure node feature discovery
+        # while ! kustomize build $KUBEFLOW_KUSTOMIZE/nfd | awk '!/well-defined/' | oc apply -f -; do echo -e "Retrying to apply resources for Node Feature Discovery..."; sleep 10; done
 
         # Install GPU Operator
         oc create namespace gpu-operator
